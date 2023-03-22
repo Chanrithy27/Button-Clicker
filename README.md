0952eca1205cf0862cf22bc5c8d99f417782a8ac
Chanrithy27 <chanrithy27@icloud.com>
2023-03-22 15:54:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coding Dojo - Button Clicker
+
+This is an assignment that showcases the use of flexbox and basic functions of Javascript.
---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(element) {
+    element.value = "Logout";
+}
+
+function message() {
+    alert("Ninja was liked!");
+}
+
+function hide(element) {
+    element.remove();
+}
